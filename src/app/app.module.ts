008f49d19bfc62368e434c4495d70ce0f0003e29
Cole Marsteller and Rohan Moore <rohan@>
2019-01-29 10:35:20 -0800

    everything

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http'
 
-
+import { routing } from './app.routing';
 import { AppComponent } from './app.component';
+import { RoverFormComponent } from './rover-form/rover-form.component';
+import { PhotosListComponent } from './photos-list/photos-list.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { masterFirebaseConfig } from './api-keys';
+import { UserPhotosListComponent } from './user-photos-list/user-photos-list.component';
 
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoverFormComponent,
+    PhotosListComponent,
+    UserPhotosListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.ts
@@ -0,0 +1,17 @@
+import { ModuleWithProviders }  from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { RoverFormComponent } from './rover-form/rover-form.component';
+import { UserPhotosListComponent } from './user-photos-list/user-photos-list.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: RoverFormComponent
+  },
+  {
+    path: 'user/photos',
+    component: UserPhotosListComponent
+  }
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
